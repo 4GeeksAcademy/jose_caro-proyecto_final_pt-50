3ab513a45b192e8c21b51ce41452ee3756eb2d77
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2025-07-12 02:00:56 +0000

    Revert "[Segundo commit frontend-register]"
    This reverts commit 8ad50cc4c97eaf47705d80d3929d1e06b9079607.

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -35,8 +35,7 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1 className="display-4">Hello Rigo!!</h1>
-			<h1 className="display-4">Commit 1 - Frontend Register</h1>
-			<h1 className="display-4">Commit 2 - Frontend Register</h1>
+			<h1 className="display-4">Commi 1 - Frontend Register</h1>
 			<p className="lead">
 				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
 			</p>
